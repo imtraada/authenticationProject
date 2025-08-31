2088cb9723ba339389cbec89a6d4286df9726dba
imtraada <melondua1@gmail.com>
2025-08-31 19:57:22 +0700

    berhasil cek api auth

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
 
     mongoose.connection.on('connected', ()=>console.log("database connected"));
 
-    await mongoose.connect(`${process.env.MONGODB_URL}/mernAuth`);
+    await mongoose.connect(`${process.env.MONGODB_URL}/mernaut`);
 };
 
 export default connectDB;
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import cookieParser from "cookie-parser";
 
 import connectDB from './config/mongodb.js';
+import authRouter from './routes/authRoutes.js';
 
 
 const app = express();
@@ -14,6 +15,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({credentials: true}))
 
+// API endpoints
 app.get('/', (req, res)=> res.send("API WORKING!!"));
+app.use('/api/auth', authRouter);
 
 app.listen(port, ()=> console.log(`server started on PORT: ${port}`));
