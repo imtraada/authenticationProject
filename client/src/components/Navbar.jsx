22e8191857797318b7de4adc7801f064d76fa343
imtraada <melondua1@gmail.com>
2025-09-01 21:39:37 +0700

    berhasil verifykasi otp

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,20 +7,38 @@ import { toast } from "react-toastify";
 
 const Navbar = () => {
   const navigate = useNavigate();
-  const {userData, backendUrl, setUserData, setIsLoggedin} = useContext(AppContext);
+  const { userData, backendUrl, setUserData, setIsLoggedin } =
+    useContext(AppContext);
+  const sendVerificationOtp = async () => {
+    try {
+      axios.defaults.withCredentials = true;
+      const { data } = await axios.post(
+        backendUrl + "/api/auth/send-verify-otp"
+      );
+      if (data.success) {
+        navigate("/email-verify");
+        toast.success(data.message);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(
+        error.response?.data?.message || "Failed to send verification otp"
+      );
+    }
+  };
 
   const logout = async () => {
-    try{
-        axios.defaults.withCredentials = true;
-        const {data} = await axios.post(backendUrl + '/api/auth/logout');
-        data.success && setIsLoggedin(false);
-        data.success && setUserData(false);
-        navigate('/');
-
-    }catch(error){
-        toast.error(error.response?.data?.message || "Failed to logout");
+    try {
+      axios.defaults.withCredentials = true;
+      const { data } = await axios.post(backendUrl + "/api/auth/logout");
+      data.success && setIsLoggedin(false);
+      data.success && setUserData(false);
+      navigate("/");
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Failed to logout");
     }
-  }
+  };
 
   return (
     <div className="w-full flex items-center justify-between p-4 sm:p-6 sm:px-24 absolute top-0 ">
@@ -30,25 +48,37 @@ const Navbar = () => {
         alt=""
         className="w-28 sm:w-32"
       />
-      {userData ?
-      <div className="w-8 h-8 flex justify-center items-center rounded-full bg-black text-white relative group">
-        {userData.name[0].toUpperCase()}
-        <div className="absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-10">
+      {userData ? (
+        <div className="w-8 h-8 flex justify-center items-center rounded-full bg-black text-white relative group">
+          {userData.name[0].toUpperCase()}
+          <div className="absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-10">
             <ul className="list-none m-0 p-2 bg-gray-100 text-sm">
-                { !userData.isAccountVerified && <li className="py-1 px-2 hover:bg-gray-200 cursor-pointer">Verify email</li> }
-                
-                <li onClick={logout} className="py-1 px-2 hover:bg-gray-200 cursor-pointer pr-10">Logout</li>
+              {!userData.isAccountVerified && (
+                <li
+                  onClick={sendVerificationOtp}
+                  className="py-1 px-2 hover:bg-gray-200 cursor-pointer"
+                >
+                  Verify email
+                </li>
+              )}
+
+              <li
+                onClick={logout}
+                className="py-1 px-2 hover:bg-gray-200 cursor-pointer pr-10"
+              >
+                Logout
+              </li>
             </ul>
+          </div>
         </div>
-      </div> 
-      : <button
-        onClick={() => navigate("/login")}
-        className="flex item-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all"
-      >
-        Login <img src={assets.arrow_icon} alt="" />{" "}
-      </button> 
-      }
-      
+      ) : (
+        <button
+          onClick={() => navigate("/login")}
+          className="flex item-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all"
+        >
+          Login <img src={assets.arrow_icon} alt="" />{" "}
+        </button>
+      )}
     </div>
   );
 };
